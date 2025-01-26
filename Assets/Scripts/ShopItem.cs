b9ccd737f52d16b36c367bb898f8aa347d00b28a
JVViacrusis <justin.viacrusis21p@gmail.com>
2025-01-26 12:11:02 -0800

    added modular shop item scripts and prefabs

---
+++ Assets/Scripts/ShopItem.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+* BuyFireRate
+* BuyDash
+* BuyMaxHealth
+* BuyMultishot
+* BuyBiggerBullet
+*/
+
+public class ShopItem : MonoBehaviour
+{
+
+    [SerializeField] GameManager.PowerUpType type;
+    [SerializeField] int cost;
+    [SerializeField] private GameManager gameManager;
+
+    private Interactable interactable;
+
+    private void Start()
+    {
+        interactable = GetComponent<Interactable>();
+        interactable.OnInteractedWith += HandleInteractedWith;
+    }
+
+    public void HandleInteractedWith(Interactable other)
+    {
+        Debug.Log("ShopItem " + name + " was interacted with by " + other.name);
+        int curOffenseTokens = gameManager.inventory[0];
+
+        if (curOffenseTokens < cost) {
+            Debug.Log("Not enough tokens to buy " + name + ". Need " + cost + " but have " + curOffenseTokens);
+            return;
+        }
+
+        gameManager.inventory[0] -= cost;
+        gameManager.AddPowerUp(type);
+
+        Debug.Log(name + " bought a " + type + " for " + cost + " tokens. Now have " + gameManager.inventory[0] + " tokens left and " + gameManager.ownedPowerUps[type] + " " + type + " powerups.");
+    }
+}
