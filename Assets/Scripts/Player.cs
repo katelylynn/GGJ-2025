2207a08cff28accae71c8581eecdedefd2c725e1
kate <katezraly@gmail.com>
2025-01-25 23:44:35 -0800

    added wizard sprite and animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,34 +3,61 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Animator animator;
+
     public float speed = 10f; 
     public float rotationSpeed = 100f;
+    private int direction;
 
     private float health = 1.0f;
     [SerializeField] public static float damageAmount = 0.1f;
     public static event Action DamageTaken;
     public static event Action PlayerDied;
 
-    private void Update()
+    private void Start()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Move(new Vector2(horizontalInput, verticalInput));
+        animator = GetComponent<Animator>();
     }
 
-    private void Move(Vector2 movementDirection)
+    private void Update()
     {
-        // identical to enemy move method
+        direction = GetDirection();
+        Debug.Log(direction);
+        Animate(direction);
+        Move();
+    }
+
+    private int GetDirection()
+    {
+        bool up = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+        bool down = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
+        bool left = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
+        bool right = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
+
+        if (up && right) return 2;  // NE
+        if (up && left) return 8;   // NW
+        if (down && right) return 4; // SE
+        if (down && left) return 6;  // SW
+        if (up) return 1;           // N
+        if (right) return 3;        // E
+        if (down) return 5;         // S
+        if (left) return 7;         // W
+        return direction;           // use previous direction
+    }
+
+    private void Animate(int dir)
+    {
+        animator.SetInteger("direction", dir);
+    }
+
+    private void Move()
+    {
+        Vector2 movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
         movementDirection.Normalize();
-
+        Debug.Log(animator.GetBool("walking"));
+        animator.SetBool("walking", movementDirection.magnitude > 0f);
         transform.Translate(movementDirection * speed * inputMagnitude * Time.deltaTime, Space.World);
-
-        if (movementDirection != Vector2.zero)
-        {
-            Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, movementDirection);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-        }
     }
 
     private void OnCollisionStay2D(Collision2D col)
